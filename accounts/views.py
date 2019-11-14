e1413b091f7eb97da5e772079eaac6f212c53077
dustyturner <samrelins@gmail.com>
2019-11-14 12:46:03 +0000

    added features to register page

--- accounts/views.py
+++ accounts/views.py
@@ -25,10 +25,16 @@ def auth_register(request):
 
     if request.method == "POST":
         username = request.POST["username"]
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
         email = request.POST["email"]
         password = request.POST["password"]
 
-        user = User.objects.create_user(username, email, password)
+        user = User.objects.create_user(username = username, 
+                                        first_name = first_name, 
+                                        last_name = last_name, 
+                                        email = email, 
+                                        password = password)
 
         login(request, user)
 
